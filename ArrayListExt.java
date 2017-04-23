d4be3a4edb316beca52b2552572b20a606453b66
saharatarey2539 <saharatarey2539@gmail.com>
2017-04-23 15:08:02 +0700

    Add files via upload

---
+++ ArrayListExt.java
@@ -0,0 +1,21 @@
+package ecp3q.Lab4;
+public class ArrayListExt extends ArraysList {
+
+	public static void main(String[] args){
+		ArrayListExt count = new ArrayListExt();
+		int sum = count.countItem(6);
+		System.out.println(sum);
+	}
+		
+	public int countItem(Object e) {
+		int count = 0;
+		for (int i = 0; i < size; i++) {
+			if (elementData[i].equals(e)) {
+				count++;
+			}
+		}
+		return count;
+	}
+	
+
+}
